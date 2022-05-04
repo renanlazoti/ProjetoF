5f419ef983bb64677acaa1d381016cc346acc90c
RENAN FERREIRA LAZOTI <renan.flazoti@senacsp.edu.br>
2022-05-04 17:29:50 -0300

    Continuação de parte do progresso do site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+function carregarplacamae(valor){
+
+    var pintel = ["Selecione a placa mãe","H110", "B560", "Z490"]
+    
+    var pamd = ["Selecione o placa mãe", "A320", "B450", "X570"]
+    
+    document.getElementById("placamae").innerHTML="";
+    if(valor == "pamd"){
+        
+        for(var i = 0; i < pamd.length;i++)
+        document.getElementById("placamae").innerHTML += `<option value=${pamd[i]}>${pamd[i]}</option>`;
+    }
+    if(valor == "pintel"){
+        for(var i = 0; i < pintel.length;i++)
+        document.getElementById("placamae").innerHTML += `<option value=${pintel[i]}>${pintel[i]}</option>`;
+    }
+}
+
 function carregarprocessador(valor){
 
 var intel = ["Selecione o processador","i3", "i5", "i7", "i9"]
@@ -18,11 +36,79 @@ if(valor == "intel"){
 
 function carregarram(valor){
 
-var memoriaram = ["Selecione a memória", "DDR2", "DDR3", "DDR4", "DDR5"]
+var memoriaram = ["Selecione a memória RAM", "DDR2", "DDR3", "DDR4", "DDR5"]
 
-if(valor == "ram"){
-    
-    for(var i = 0; i < ram.length;i++)
-    document.getElementById("ram").innerHTML += `<option value=${ram[i]}>${ram[i]}</option>`;
+if(valor == "memoriaram"){
+
+    for(var i = 0; i < memoriaram.length;i++)
+    document.getElementById("mem").innerHTML += `<option value=${memoriaram[i]}>${memoriaram[i]}</option>`;
 }
+}
+
+function carregarvideo(valor){
+
+    var placadevideo = ["Selecione a memória RAM", "DDR2", "DDR3", "DDR4", "DDR5"]
+    
+    if(valor == "placadevideo"){
+    
+        for(var i = 0; i < placadevideo.length;i++)
+        document.getElementById("placadevideo").innerHTML += `<option value=${placadevideo[i]}>${placadevideo[i]}</option>`;
+    }
+    }
+
+function carregarcooler(valor){
+
+    var cooler = ["Selecione o cooler", "Water cooler 120mm", "Water cooler 240mm", "Water cooler 360mm", "Air cooler"]
+    
+    if(valor == "cooler"){
+    
+        for(var i = 0; i < cooler.length;i++)
+        document.getElementById("cooler").innerHTML += `<option value=${cooler[i]}>${cooler[i]}</option>`;
+    }
+}
+
+function carregarfonte(valor){
+
+    var fonte = ["Selecione a fonte", "300w", "400w", "500w", "600w", "700w", "800w", "900w", "1000w"]
+    
+    if(valor == "fonte"){
+    
+        for(var i = 0; i < fonte.length;i++)
+        document.getElementById("fonte").innerHTML += `<option value=${fonte[i]}>${fonte[i]}</option>`;
+    }
+}
+    
+function carregararmazenamento(valor){
+
+    var ssd = ["Selecione o tamanho de armazenamento","120gb", "240gb", "480gb", "512gb", "960gb"]
+    
+    var hd = ["Selecione o tamanho de armazenamento","500gb", "1tb", "2tb"]
+
+    var nvme = ["Selecione o tamanho de armazenamento","120gb", "240gb", "480gb", "512gb", "1tb"]
+    
+    document.getElementById("armazenamento").innerHTML="";
+    if(valor == "ssd"){
+        
+        for(var i = 0; i < ssd.length;i++)
+        document.getElementById("armazenamento").innerHTML += `<option value=${ssd[i]}>${ssd[i]}</option>`;
+    }
+    if(valor == "hd"){
+        for(var i = 0; i < hd.length;i++)
+        document.getElementById("armazenamento").innerHTML += `<option value=${hd[i]}>${hd[i]}</option>`;
+    }
+    if(valor == "nvme"){
+        for(var i = 0; i < nvme.length;i++)
+        document.getElementById("armazenamento").innerHTML += `<option value=${nvme[i]}>${nvme[i]}</option>`;
+    }
+}
+
+function carregargabinete(valor){
+
+    var gabinete = ["Selecione o gabinete", "Mini-tower", "Mid-tower", "Full-tower"]
+    
+    if(valor == "gabinete"){
+    
+        for(var i = 0; i < gabinete.length;i++)
+        document.getElementById("gabinete").innerHTML += `<option value=${gabinete[i]}>${gabinete[i]}</option>`;
+    }
 }
